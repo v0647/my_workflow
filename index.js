02a9228e96bb9bd31dc2ae392c1ad4c70ba9e3cf
v0647 <vipin.v@bahwancybertek.com>
2023-02-11 13:02:12 +0530

    testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-console.log('sddfdf');
+console.log('dddsddfdf');
 AppRegistry.registerComponent(appName, () => App);
